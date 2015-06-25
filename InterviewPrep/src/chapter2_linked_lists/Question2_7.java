103689e68da8c38e195344cfd7287e0c52cf148f
Daniel J Montanez <dmontanez86@gmail.com>
2015-06-24 19:34:44 -0700

    Partial Work for 2.7

--- InterviewPrep/src/chapter2_linked_lists/Question2_7.java
+++ InterviewPrep/src/chapter2_linked_lists/Question2_7.java
@@ -1,7 +1,44 @@
 package chapter2_linked_lists;
 
+import dataStructures.Node;
+
 public class Question2_7 {
 /*
  * Implement a function to determine if a linked list is a palindrome.
  */
+	
+	// RACECAR
+	// reverse list, compare nodes.
+	
+	public Node n;
+	
+	public Question2_7(int[] list) {
+		n = new Node(list[0]);
+		for(int i = 1; i <list.length; i++) {
+			Node node = new Node(list[i]);
+			n.appendToTail(node);
+		}
+	}
+	
+	public boolean isPalendrome() {
+		Node reverse = reverseList();
+		while(n != null) {
+			if(n.data != reverse.data) return false;
+			n = n.next;
+			reverse = reverse.next;
+		}
+		return true;
+	}
+
+	public Node reverseList() {
+		Node flipped = new Node(n.data);
+		n = n.next;
+		while(n != null) {
+			flipped.appendToHead(n);
+			n = n.next;
+		}
+		
+		return flipped;
+	}
+	
 }
